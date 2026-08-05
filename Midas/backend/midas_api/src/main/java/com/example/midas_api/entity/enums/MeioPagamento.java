7c1093af681cb1f7bed95048f0b39964c5a5c147
Pedro Yada Barbeitos <pedrobarbeitos-ieg@instituto.ad>
2026-08-05 11:46:36 -0300

    feat: yada - criando enums das entitys

---
+++ Midas/backend/midas_api/src/main/java/com/example/midas_api/entity/enums/MeioPagamento.java
@@ -0,0 +1,8 @@
+package com.example.midas.entity.enums;
+
+public enum MeioPagamento {
+    PIX,
+    CARTAO_CREDITO,
+    CARTAO_DEBITO,
+    BOLETO
+}
---
+++ Midas/backend/midas_api/src/main/java/com/example/midas_api/entity/enums/StatusUsuario.java
@@ -0,0 +1,6 @@
+package com.example.midas.entity.enums;
+
+public enum StatusUsuario {
+    ATIVO,
+    INATIVO
+}
