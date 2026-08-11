1c0dc071c74645389d2df5515cf08690b4f834bd
Pedro Yada Barbeitos <pedrobarbeitos-ieg@instituto.ad>
2026-08-10 22:46:25 -0300

    feat: yada - criando as entidades de favorito de acordo com a nova modelagem

--- Midas/backend/midas_api/src/main/java/com/example/midas_api/entity/FavoritoId.java
+++ Midas/backend/midas_api/src/main/java/com/example/midas_api/entity/FavoritoId.java
@@ -1,4 +1,45 @@
 package com.example.midas_api.entity;
 
-public class FavoritoId {
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+public class FavoritoId implements Serializable {
+
+    private Integer usuarioId;
+    private Integer leilaoId;
+
+    public Integer getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(Integer usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public Integer getLeilaoId() {
+        return leilaoId;
+    }
+
+    public void setLeilaoId(Integer leilaoId) {
+        this.leilaoId = leilaoId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof FavoritoId that)) return false;
+        return Objects.equals(usuarioId, that.usuarioId) && Objects.equals(leilaoId, that.leilaoId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(usuarioId, leilaoId);
+    }
 }
