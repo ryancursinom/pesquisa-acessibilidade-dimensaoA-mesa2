d6d6c6c28708093fd152e30e458060d020969637
Matheus Rezende Nascimento Raimundo <matheusraimundo-ieg@instituto.ad>
2026-08-12 11:04:41 -0300

    feat: Matheus - produto_repository, em repositories

--- Midas/ai/app/repositories/produto_repository.py
+++ Midas/ai/app/repositories/produto_repository.py
@@ -1,14 +1,75 @@
-from sqlalchemy.orm import Session
-
-from app.models.produto import Produto
+from app.database import conectar
 
 
-class ProdutoRepository:
-    def __init__(self, db: Session):
-        self.db = db
+def buscar_produto(produto_id: int):
 
-    def listar_todos(self):
-        return self.db.query(Produto).all()
+    conexao = conectar()
 
-    def buscar_por_id(self, produto_id: int):
-        return self.db.query(Produto).filter(Produto.id == produto_id).first()
+    try:
+        with conexao.cursor() as cursor:
+
+            cursor.execute(
+                """
+                SELECT
+                    id,
+                    nome,
+                    imagem_url
+                FROM produto
+                WHERE id = %s
+                """,
+                (produto_id,)
+            )
+
+            resultado = cursor.fetchone()
+
+            if not resultado:
+                return None
+
+            return {
+                "id": resultado[0],
+                "nome": resultado[1],
+                "imagem_url": resultado[2]
+            }
+
+    finally:
+        conexao.close()
+
+
+def buscar_identidade_visual(produto_id: int):
+
+    conexao = conectar()
+
+    try:
+        with conexao.cursor() as cursor:
+
+            cursor.execute(
+                """
+                SELECT
+                    cor_primaria,
+                    cor_secundaria,
+                    descricao_paleta,
+                    formato,
+                    descricao_formato,
+                    descricao_geral
+                FROM identidade_visual
+                WHERE produto_id = %s
+                """,
+                (produto_id,)
+            )
+
+            resultado = cursor.fetchone()
+
+            if not resultado:
+                return None
+
+            return {
+                "cor_primaria": resultado[0],
+                "cor_secundaria": resultado[1],
+                "descricao_paleta": resultado[2],
+                "formato": resultado[3],
+                "descricao_formato": resultado[4],
+                "descricao_geral": resultado[5]
+            }
+    
+    finally:
+        conexao.close()
